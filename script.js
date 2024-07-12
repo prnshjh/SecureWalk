3c9cfe89c1564003e165f9975574f4297011dea6
Priyanshu Kumar <116969540+prnshjh@users.noreply.github.com>
2024-07-12 21:26:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,613 @@
 const citymap = {
-    delhi: {
-      center: { lat: 28.6139, lng: 77.2090 },
-      radi: 20000, 
-    },
-    gurugram: {
-      center: { lat: 28.4595, lng: 77.0266 },
-      radi: 15000, 
-    },
-    noida: {
-      center: { lat: 28.5355, lng: 77.3910 },
-      radi: 15000, 
-    },
-    mumbai: {
-      center: { lat: 19.0760, lng: 72.8777 },
-      radi: 10000, 
-    },
-    bangalore: {
-      center: { lat: 12.9716, lng: 77.5946 },
-      radi: 10000, 
-    },
-    kolkata: {
-      center: { lat: 22.5726, lng: 88.3639 },
-      radi: 10000, 
-    },
-    jaipur: {
-      center: { lat: 26.9124, lng: 75.7873 },
-      radi: 10000, 
-    },
-    lucknow: {
-      center: { lat: 26.8467, lng: 80.9462 },
-      radi: 15000, 
-    },
-    patna: {
-      center: { lat: 25.5941, lng: 85.1376 },
-      radi: 15000, 
-    },
-    hyderabad: {
-      center: { lat: 17.3850, lng: 78.4867 },
-      radi: 10000, 
-    }
-  };
-  
+  dhaula_kuan_crossing: {
+    center: { lat: 28.5957, lng: 77.1553 },
+    radi: 20000,
+  },
+  nelson_mandela_marg_vasant_kunj: {
+    center: { lat: 28.5165, lng: 77.1646 },
+    radi: 15000,
+  },
+  benito_juarez_marg_south_campus: {
+    center: { lat: 28.5831, lng: 77.1623 },
+    radi: 15000,
+  },
+  paharganj: {
+    center: { lat: 28.6416, lng: 77.2167 },
+    radi: 10000,
+  },
+  mehrauli_gurgaon_road: {
+    center: { lat: 28.5027, lng: 77.0910 },
+    radi: 10000,
+  },
+  ghitorni_metro_station: {
+    center: { lat: 28.4820, lng: 77.1442 },
+    radi: 10000,
+  },
+  sarita_vihar: {
+    center: { lat: 28.5292, lng: 77.2925 },
+    radi: 10000,
+  },
+  karol_bagh: {
+    center: { lat: 28.6515, lng: 77.1882 },
+    radi: 10000,
+  },
+  lado_sarai: {
+    center: { lat: 28.5294, lng: 77.1926 },
+    radi: 10000,
+  },
+  mathura_road_surajkund_bypass: {
+    center: { lat: 28.4801, lng: 77.2862 },
+    radi: 10000,
+  },
+  kurla_outside_phoenix_marketcity: {
+    center: { lat: 19.0728, lng: 72.8795 },
+    radi: 10000,
+  },
+  sakinaka: {
+    center: { lat: 19.0952, lng: 72.8862 },
+    radi: 10000,
+  },
+  borivali_gorai_beach_area: {
+    center: { lat: 19.2183, lng: 72.8593 },
+    radi: 10000,
+  },
+  colaba_near_gateway_of_india: {
+    center: { lat: 18.9220, lng: 72.8347 },
+    radi: 10000,
+  },
+  andheri_western_express_highway: {
+    center: { lat: 19.1197, lng: 72.8475 },
+    radi: 10000,
+  },
+  jogeshwari_behram_baug: {
+    center: { lat: 19.1408, lng: 72.8405 },
+    radi: 10000,
+  },
+  dadar_shivaji_park: {
+    center: { lat: 19.0269, lng: 72.8384 },
+    radi: 10000,
+  },
+  ghatkopar_asalpha_village: {
+    center: { lat: 19.1033, lng: 72.9067 },
+    radi: 10000,
+  },
+  santacruz_juhu_beach_area: {
+    center: { lat: 19.1037, lng: 72.8266 },
+    radi: 10000,
+  },
+  malad_mindspace: {
+    center: { lat: 19.1742, lng: 72.8424 },
+    radi: 10000,
+  },
+  majestic_bus_stand_area: {
+    center: { lat: 12.9784, lng: 77.5718 },
+    radi: 10000,
+  },
+  koramangala_near_forum_mall: {
+    center: { lat: 12.9352, lng: 77.6245 },
+    radi: 10000,
+  },
+  whitefield_itpl_road: {
+    center: { lat: 12.9698, lng: 77.7499 },
+    radi: 10000,
+  },
+  mg_road_brigade_road_junction: {
+    center: { lat: 12.9716, lng: 77.5946 },
+    radi: 10000,
+  },
+  indiranagar_100_feet_road: {
+    center: { lat: 12.9716, lng: 77.6408 },
+    radi: 10000,
+  },
+  electronic_city_phase_1_and_2: {
+    center: { lat: 12.8411, lng: 77.6737 },
+    radi: 10000,
+  },
+  btm_layout_outer_ring_road: {
+    center: { lat: 12.9166, lng: 77.6101 },
+    radi: 10000,
+  },
+  kr_puram_hoodi_circle: {
+    center: { lat: 12.9904, lng: 77.6958 },
+    radi: 10000,
+  },
+  yeshwanthpur_railway_station_area: {
+    center: { lat: 13.0285, lng: 77.5411 },
+    radi: 10000,
+  },
+  bellandur_ecospace_area: {
+    center: { lat: 12.9279, lng: 77.6842 },
+    radi: 10000,
+  },
+  park_street_near_allen_park: {
+    center: { lat: 22.5521, lng: 88.3538 },
+    radi: 10000,
+  },
+  salt_lake_sector_v: {
+    center: { lat: 22.5685, lng: 88.4315 },
+    radi: 10000,
+  },
+  new_market_lindsay_street: {
+    center: { lat: 22.5615, lng: 88.3550 },
+    radi: 10000,
+  },
+  sealdah_station_area: {
+    center: { lat: 22.5710, lng: 88.3707 },
+    radi: 10000,
+  },
+  gariahat_golpark_area: {
+    center: { lat: 22.5125, lng: 88.3652 },
+    radi: 10000,
+  },
+  esplanade_dharmatala: {
+    center: { lat: 22.5596, lng: 88.3518 },
+    radi: 10000,
+  },
+  howrah_station_area: {
+    center: { lat: 22.5892, lng: 88.3428 },
+    radi: 10000,
+  },
+  jadavpur_8b_bus_stand_area: {
+    center: { lat: 22.4980, lng: 88.3714 },
+    radi: 10000,
+  },
+  burrabazar: {
+    center: { lat: 22.5820, lng: 88.3484 },
+    radi: 10000,
+  },
+  behala_chowrasta: {
+    center: { lat: 22.4873, lng: 88.2960 },
+    radi: 10000,
+  },
+  t_nagar_pondy_bazaar: {
+    center: { lat: 13.0406, lng: 80.2337 },
+    radi: 10000,
+  },
+  egmore_station_area: {
+    center: { lat: 13.0753, lng: 80.2572 },
+    radi: 10000,
+  },
+  kodambakkam_arcot_road: {
+    center: { lat: 13.0518, lng: 80.2345 },
+    radi: 10000,
+  },
+  tambaram_bus_stand_area: {
+    center: { lat: 12.9229, lng: 80.1270 },
+    radi: 10000,
+  },
+  velachery_phoenix_marketcity_area: {
+    center: { lat: 12.9834, lng: 80.2207 },
+    radi: 10000,
+  },
+  mylapore_luz_corner: {
+    center: { lat: 13.0348, lng: 80.2689 },
+    radi: 10000,
+  },
+  anna_nagar_second_avenue: {
+    center: { lat: 13.0838, lng: 80.2117 },
+    radi: 10000,
+  },
+  guindy_kathipara_junction: {
+    center: { lat: 13.0107, lng: 80.2150 },
+    radi: 10000,
+  },
+  besant_nagar_elliots_beach: {
+    center: { lat: 12.9982, lng: 80.2707 },
+    radi: 10000,
+  },
+  nungambakkam_sterling_road: {
+    center: { lat: 13.0601, lng: 80.2389 },
+    radi: 10000,
+  },
+  ameerpet_satyam_theater_area: {
+    center: { lat: 17.4360, lng: 78.4489 },
+    radi: 10000,
+  },
+  const citymap = {
+  dhaula_kuan_crossing: {
+    center: { lat: 28.5957, lng: 77.1553 },
+    radi: 20000,
+  },
+  nelson_mandela_marg_vasant_kunj: {
+    center: { lat: 28.5165, lng: 77.1646 },
+    radi: 15000,
+  },
+  benito_juarez_marg_south_campus: {
+    center: { lat: 28.5831, lng: 77.1623 },
+    radi: 15000,
+  },
+  paharganj: {
+    center: { lat: 28.6416, lng: 77.2167 },
+    radi: 10000,
+  },
+  mehrauli_gurgaon_road: {
+    center: { lat: 28.5027, lng: 77.0910 },
+    radi: 10000,
+  },
+  ghitorni_metro_station: {
+    center: { lat: 28.4820, lng: 77.1442 },
+    radi: 10000,
+  },
+  sarita_vihar: {
+    center: { lat: 28.5292, lng: 77.2925 },
+    radi: 10000,
+  },
+  karol_bagh: {
+    center: { lat: 28.6515, lng: 77.1882 },
+    radi: 10000,
+  },
+  lado_sarai: {
+    center: { lat: 28.5294, lng: 77.1926 },
+    radi: 10000,
+  },
+  mathura_road_surajkund_bypass: {
+    center: { lat: 28.4801, lng: 77.2862 },
+    radi: 10000,
+  },
+  kurla_outside_phoenix_marketcity: {
+    center: { lat: 19.0728, lng: 72.8795 },
+    radi: 10000,
+  },
+  sakinaka: {
+    center: { lat: 19.0952, lng: 72.8862 },
+    radi: 10000,
+  },
+  borivali_gorai_beach_area: {
+    center: { lat: 19.2183, lng: 72.8593 },
+    radi: 10000,
+  },
+  colaba_near_gateway_of_india: {
+    center: { lat: 18.9220, lng: 72.8347 },
+    radi: 10000,
+  },
+  andheri_western_express_highway: {
+    center: { lat: 19.1197, lng: 72.8475 },
+    radi: 10000,
+  },
+  jogeshwari_behram_baug: {
+    center: { lat: 19.1408, lng: 72.8405 },
+    radi: 10000,
+  },
+  dadar_shivaji_park: {
+    center: { lat: 19.0269, lng: 72.8384 },
+    radi: 10000,
+  },
+  ghatkopar_asalpha_village: {
+    center: { lat: 19.1033, lng: 72.9067 },
+    radi: 10000,
+  },
+  santacruz_juhu_beach_area: {
+    center: { lat: 19.1037, lng: 72.8266 },
+    radi: 10000,
+  },
+  malad_mindspace: {
+    center: { lat: 19.1742, lng: 72.8424 },
+    radi: 10000,
+  },
+  majestic_bus_stand_area: {
+    center: { lat: 12.9784, lng: 77.5718 },
+    radi: 10000,
+  },
+  koramangala_near_forum_mall: {
+    center: { lat: 12.9352, lng: 77.6245 },
+    radi: 10000,
+  },
+  whitefield_itpl_road: {
+    center: { lat: 12.9698, lng: 77.7499 },
+    radi: 10000,
+  },
+  mg_road_brigade_road_junction: {
+    center: { lat: 12.9716, lng: 77.5946 },
+    radi: 10000,
+  },
+  indiranagar_100_feet_road: {
+    center: { lat: 12.9716, lng: 77.6408 },
+    radi: 10000,
+  },
+  electronic_city_phase_1_and_2: {
+    center: { lat: 12.8411, lng: 77.6737 },
+    radi: 10000,
+  },
+  btm_layout_outer_ring_road: {
+    center: { lat: 12.9166, lng: 77.6101 },
+    radi: 10000,
+  },
+  kr_puram_hoodi_circle: {
+    center: { lat: 12.9904, lng: 77.6958 },
+    radi: 10000,
+  },
+  yeshwanthpur_railway_station_area: {
+    center: { lat: 13.0285, lng: 77.5411 },
+    radi: 10000,
+  },
+  bellandur_ecospace_area: {
+    center: { lat: 12.9279, lng: 77.6842 },
+    radi: 10000,
+  },
+  park_street_near_allen_park: {
+    center: { lat: 22.5521, lng: 88.3538 },
+    radi: 10000,
+  },
+  salt_lake_sector_v: {
+    center: { lat: 22.5685, lng: 88.4315 },
+    radi: 10000,
+  },
+  new_market_lindsay_street: {
+    center: { lat: 22.5615, lng: 88.3550 },
+    radi: 10000,
+  },
+  sealdah_station_area: {
+    center: { lat: 22.5710, lng: 88.3707 },
+    radi: 10000,
+  },
+  gariahat_golpark_area: {
+    center: { lat: 22.5125, lng: 88.3652 },
+    radi: 10000,
+  },
+  esplanade_dharmatala: {
+    center: { lat: 22.5596, lng: 88.3518 },
+    radi: 10000,
+  },
+  howrah_station_area: {
+    center: { lat: 22.5892, lng: 88.3428 },
+    radi: 10000,
+  },
+  jadavpur_8b_bus_stand_area: {
+    center: { lat: 22.4980, lng: 88.3714 },
+    radi: 10000,
+  },
+  burrabazar: {
+    center: { lat: 22.5820, lng: 88.3484 },
+    radi: 10000,
+  },
+  behala_chowrasta: {
+    center: { lat: 22.4873, lng: 88.2960 },
+    radi: 10000,
+  },
+  t_nagar_pondy_bazaar: {
+    center: { lat: 13.0406, lng: 80.2337 },
+    radi: 10000,
+  },
+  egmore_station_area: {
+    center: { lat: 13.0753, lng: 80.2572 },
+    radi: 10000,
+  },
+  kodambakkam_arcot_road: {
+    center: { lat: 13.0518, lng: 80.2345 },
+    radi: 10000,
+  },
+  tambaram_bus_stand_area: {
+    center: { lat: 12.9229, lng: 80.1270 },
+    radi: 10000,
+  },
+  velachery_phoenix_marketcity_area: {
+    center: { lat: 12.9834, lng: 80.2207 },
+    radi: 10000,
+  },
+  mylapore_luz_corner: {
+    center: { lat: 13.0348, lng: 80.2689 },
+    radi: 10000,
+  },
+  anna_nagar_second_avenue: {
+    center: { lat: 13.0838, lng: 80.2117 },
+    radi: 10000,
+  },
+  guindy_kathipara_junction: {
+    center: { lat: 13.0107, lng: 80.2150 },
+    radi: 10000,
+  },
+  besant_nagar_elliots_beach: {
+    center: { lat: 12.9982, lng: 80.2707 },
+    radi: 10000,
+  },
+  nungambakkam_sterling_road: {
+    center: { lat: 13.0601, lng: 80.2389 },
+    radi: 10000,
+  },
+  ameerpet_satyam_theater_area: {
+    center: { lat: 17.4360, lng: 78.4489 },
+    radi: 10000,
+  },
+  begumpet_old_airport_road: {
+    center: { lat: 17.4483, lng: 78.4625 },
+    radi: 10000,
+  },
+    himayat_nagar_near_basheer_bagh: {
+    center: { lat: 17.4037, lng: 78.4829 },
+    radi: 10000,
+  },
+  banjara_hills_road_no_1: {
+    center: { lat: 17.4105, lng: 78.4481 },
+    radi: 10000,
+  },
+  jubilee_hills_road_no_36: {
+    center: { lat: 17.4326, lng: 78.4070 },
+    radi: 10000,
+  },
+  gachibowli_near_wipro_circle: {
+    center: { lat: 17.4457, lng: 78.3481 },
+    radi: 10000,
+  },
+  kondapur_near_botanical_garden: {
+    center: { lat: 17.4721, lng: 78.3665 },
+    radi: 10000,
+  },
+  hitech_city_cyber_towers: {
+    center: { lat: 17.4483, lng: 78.3857 },
+    radi: 10000,
+  },
+  charminar_near_lad_bazaar: {
+    center: { lat: 17.3616, lng: 78.4747 },
+    radi: 10000,
+  },
+  lb_nagar_ring_road: {
+    center: { lat: 17.3487, lng: 78.5583 },
+    radi: 10000,
+  },
+  mehdipatnam_near_pillar_no_56: {
+    center: { lat: 17.3871, lng: 78.4421 },
+    radi: 10000,
+  },
+  hitech_city_madhapur: {
+    center: { lat: 17.4480, lng: 78.3915 },
+    radi: 10000,
+  },
+  gachibowli_dlf_building: {
+    center: { lat: 17.4503, lng: 78.3824 },
+    radi: 10000,
+  },
+  hyderabad_airport_area: {
+    center: { lat: 17.2403, lng: 78.4294 },
+    radi: 10000,
+  },
+  srinagar_colony_near_naveen_nagar: {
+    center: { lat: 17.4273, lng: 78.4297 },
+    radi: 10000,
+  },
+  gachibowli_near_oracle: {
+    center: { lat: 17.4400, lng: 78.3561 },
+    radi: 10000,
+  },
+  ranigunj_hussain_sagar_lake_area: {
+    center: { lat: 17.4352, lng: 78.4867 },
+    radi: 10000,
+  },
+  karimnagar_bus_stand_area: {
+    center: { lat: 18.4330, lng: 79.1321 },
+    radi: 10000,
+  },
+  patna_boring_road: {
+    center: { lat: 25.6135, lng: 85.1350 },
+    radi: 10000,
+  },
+  patna_railway_station_area: {
+    center: { lat: 25.6022, lng: 85.1415 },
+    radi: 10000,
+  },
+  danapur_near_digha_ghat: {
+    center: { lat: 25.6387, lng: 85.0479 },
+    radi: 10000,
+  },
+  kankarbagh_near_rajendra_nagar: {
+    center: { lat: 25.6095, lng: 85.1583 },
+    radi: 10000,
+  },
+  gandhi_maidan_area: {
+    center: { lat: 25.6198, lng: 85.1425 },
+    radi: 10000,
+  },
+  danapur_cantt: {
+    center: { lat: 25.6307, lng: 85.0451 },
+    radi: 10000,
+  },
+  ashiana_digha_road: {
+    center: { lat: 25.6153, lng: 85.0487 },
+    radi: 10000,
+  },
+  anishabad_near_bihar_assembly: {
+    center: { lat: 25.5900, lng: 85.1376 },
+    radi: 10000,
+  },
+  phulwari_sharif_railway_station_area: {
+    center: { lat: 25.5690, lng: 85.0676 },
+    radi: 10000,
+  },
+  patna_university_area: {
+    center: { lat: 25.6182, lng: 85.1465 },
+    radi: 10000,
+  },
+  jaipur_johari_bazar: {
+    center: { lat: 26.9220, lng: 75.8235 },
+    radi: 10000,
+  },
+  amer_fort: {
+    center: { lat: 26.9855, lng: 75.8513 },
+    radi: 10000,
+  },
+  vaishali_nagar: {
+    center: { lat: 26.9224, lng: 75.7408 },
+    radi: 10000,
+  },
+  mansarovar: {
+    center: { lat: 26.8583, lng: 75.7613 },
+    radi: 10000,
+  },
+  malviya_nagar: {
+    center: { lat: 26.8500, lng: 75.8189 },
+    radi: 10000,
+  },
+  jagatpura: {
+    center: { lat: 26.8510, lng: 75.8244 },
+    radi: 10000,
+  },
+  banipark: {
+    center: { lat: 26.9361, lng: 75.7982 },
+    radi: 10000,
+  },
+  c_scheme: {
+    center: { lat: 26.9153, lng: 75.8069 },
+    radi: 10000,
+  },
+  jal_mahal: {
+    center: { lat: 26.9512, lng: 75.8405 },
+    radi: 10000,
+  },
+  gopalpura_bypass: {
+    center: { lat: 26.8647, lng: 75.7870 },
+    radi: 10000,
+  },
+  delhi_road: {
+    center: { lat: 26.8789, lng: 75.7680 },
+    radi: 10000,
+  },
+  lucknow_hazratganj: {
+    center: { lat: 26.8486, lng: 80.9462 },
+    radi: 15000,
+  },
+  lucknow_charbagh_station: {
+    center: { lat: 26.8381, lng: 80.9203 },
+    radi: 15000,
+  },
+  lucknow_aminabad: {
+    center: { lat: 26.8548, lng: 80.9344 },
+    radi: 15000,
+  },
+  lucknow_alambagh: {
+    center: { lat: 26.8142, lng: 80.9182 },
+    radi: 15000,
+  },
+  lucknow_gomti_nagar: {
+    center: { lat: 26.8500, lng: 81.0000 },
+    radi: 15000,
+  },
+  lucknow_rana_pratap_marg: {
+    center: { lat: 26.8579, lng: 80.9515 },
+    radi: 15000,
+  },
+  lucknow_indira_nagar: {
+    center: { lat: 26.8761, lng: 81.0000 },
+    radi: 15000,
+  },
+  lucknow_hazratganj_crossing: {
+    center: { lat: 26.8510, lng: 80.9462 },
+    radi: 15000,
+  },
+  lucknow_kanpur_road: {
+    center: { lat: 26.7879, lng: 80.8925 },
+    radi: 15000,
+  },
+  lucknow_mohanlalganj: {
+    center: { lat: 26.6207, lng: 81.0013 },
+    radi: 15000,
+  },
+  lucknow_rewa_road: {
+    center: { lat: 26.8707, lng: 81.0346 },
+    radi: 15000,
+  }
+};
+
+
+
   function getCurrentPosition() {
     
     if (navigator.geolocation) {
